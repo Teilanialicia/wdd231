53f54482d9eb585ca1252a371b3aa2cd36b62e98
Teilanialicia <126744697+Teilanialicia@users.noreply.github.com>
2025-03-07 18:20:14 -0800

    First commit

---
+++ script/date.js
@@ -0,0 +1,11 @@
+// creates a new variable to store the date object in 
+const date = new Date();
+// getting the year from the date object, using a "method"
+let year = date.getFullYear();
+let copyrightText = year + "©"
+
+// getting the element I want to change with the copyright year
+document.getElementById("copyright").innerText = copyrightText
+
+// getting the element I want to change with the date last modified 
+document.getElementById("lastModified").innerText = document.lastModified
